5d7742add6b107796b9b4498689a087927bc1c61
Lambo215 <martin.espejo.bhn@gmail.com>
2022-09-05 18:08:46 -0300

    segundo edit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function cambiar_parrafo() {
+function cambiar_acercade() {
     document.getElementById("edit-acercade").style.display="block";
     let texto = document.getElementById("text-acercade").innerText;
+    console.log(texto);
+}
+
+function cambiar_experiencia() {
+    document.getElementById("edit-experiencia").style.display="block";
+    let texto = document.getElementById("text-experiencia").innerText;
+    console.log(texto);
+}
+
+function cambiar_educacion() {
+    document.getElementById("edit-educacion").style.display="block";
+    let texto = document.getElementById("text-educacion").innerText;
+    console.log(texto);
+}
+
+function cambiar_proyectos() {
+    document.getElementById("edit-proyectos").style.display="block";
+    let texto = document.getElementById("text-proyectos").innerText;
     console.log(texto);
 }
